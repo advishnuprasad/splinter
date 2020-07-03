7dcc17502d746ef3fb4ff799d3b1d32dabd904ef
A D Vishnu Prasad <advishnuprasad@gmail.com>
2020-07-03 20:12:46 +0530

    Update chrome.py

--- splinter/driver/webdriver/chrome.py
+++ splinter/driver/webdriver/chrome.py
@@ -38,6 +38,7 @@ class WebDriver(BaseWebDriver):
 
         if headless:
             options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
             options.add_argument("--disable-gpu")
 
         self.driver = Chrome(options=options, **kwargs)
